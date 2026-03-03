a2bea9334d4f1defc62469d310fbe9aedb1a70f6
Ann00012 <ann.malchevska@gmail.com>
2026-03-03 21:37:34 +0200

    add Authentication

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser,loginUser, logoutUser, } from '../controllers/AuthController.js';
-import { registerUserSchema,loginUserSchema } from '../validations/AuthValidation.js';
+import { registerUser,loginUser, logoutUser, refreshUserSession } from '../controllers/AuthController.js';
+import { registerUserSchema, loginUserSchema } from '../validations/AuthValidation.js';
+
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
 
 export default router;
