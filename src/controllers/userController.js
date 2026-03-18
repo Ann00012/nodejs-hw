f02dffa3348f455ff4e7e2955a1966192db62dd5
AnnaMalchevska <ann.malchevska@gmail.com>
2026-03-18 20:17:16 +0200

    Refactor updateUserAvatar for better error handling

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,23 +3,28 @@ import { User } from '../models/user.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 export const updateUserAvatar = async (req, res, next) => {
-  if (!req.file) {
-    throw createHttpError(400, 'No file');
+  try {
+    if (!req.file) {
+      throw createHttpError(400, 'No file');
+    }
+
+    const result = await saveFileToCloudinary(req.file.buffer);
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar: result.secure_url },
+      { returnDocument: 'after' }, 
+    );
+
+    if (!user) {
+      throw createHttpError(404, 'User not found');
+    }
+
+    res.status(200).json({
+      url: user.avatar
+    });
+
+  } catch (error) {
+    next(error);
   }
-
-  const result = await saveFileToCloudinary(req.file.buffer);
-
-  const user = await User.findByIdAndUpdate(
-    req.user._id,
-    { avatar: result.secure_url },
-    { returnDocument: 'after' },
-  );
-
-  if (!user) {
-    throw createHttpError(404, 'User not found');
-  }
-
-  res.status(200).json({
-    url: user.avatar
-  });
 };
